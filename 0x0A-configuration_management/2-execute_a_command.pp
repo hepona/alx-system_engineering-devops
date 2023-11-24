f1d14493a790d94f3970e35efcfbe2d8af98af23
hepona <hepona10@gmail.com>
2023-11-24 23:06:04 +0100

    2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,4 @@
 #manifest that kills a process named killmenow.
 exec {'pkill':
-path     => '/usr/bin/',
-command  => 'pkill -f Killmenow',
-unless   => 'pkill -0 -f Killmenow'
+command => '/bin/pkill killmenow'
 }
