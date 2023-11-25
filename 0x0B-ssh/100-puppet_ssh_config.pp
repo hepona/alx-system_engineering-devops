65f14a470de0c587929446a386cd0aa369ef7c0d
hepona <hepona10@gmail.com>
2023-11-25 23:57:45 +0100

    100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Automating my Tasks using Puppet
+file { '/etc/ssh/ssh_config':
+ensure => 'present',
+content => "
+  IdentityFile ~/ssh/school
+  PasswordAuthentication no
+"
+}
