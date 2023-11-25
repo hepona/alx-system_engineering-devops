f34322a3d515e13c9e9f0321cc1450a8e1a3bfd0
hepona <hepona10@gmail.com>
2023-11-26 00:03:32 +0100

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # Automating my Tasks using Puppet
-
 file { '/etc/ssh/ssh_config':
-  ensure  => present,
+ensure => present,
 content => "
-    # SSH client configuration
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
+  Host *
+    IdentityFile ~/ssh/school
+    PasswordAuthentication no
+"
 }
