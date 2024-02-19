f7486f3cf2c8dd8f0447de95a93d43022f4e2fc6
hepona <hepona10@gmail.com>
2024-02-19 22:38:52 +0100

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,12 +5,12 @@ import requests as r
 from sys import argv
 
 if __name__ == "__main__":
-    filename = "USER_ID.csv"
-    id = argv[1]
+    filename = argv[1]+".csv"
 
-    if len(argv) != 2 or not id.isdigit():
-        print("Usage: ./0-gather_data_from_an_API <number>")
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: ./1-export_to_CSV <number>")
     else:
+        id = argv[1]
         userdata = r.get(f"https://jsonplaceholder.typicode.com/users/{id}")
         todouser = r.get(
             f"https://jsonplaceholder.typicode.com/\
@@ -18,8 +18,6 @@ todos/?userId={id}"
             )
         user = json.loads(userdata.text)
         todo = json.loads(todouser.text)
-        c_completed_task = [task for task in todo if task["completed"] is True]
-        c = 0
         username = user['username']
         with open(filename, 'w') as f:
             for i in todo:
