30e278dd0d40731628d5e510647f94db84187713
hepona <hepona10@gmail.com>
2024-02-19 20:56:30 +0100

    00

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,23 +6,21 @@ from sys import argv
 
 if __name__ == "__main__":
     id = argv[1]
-    try:
-        if len(argv) != 2 or not id.isdigit():
-            print("Usage: ./0-gather_data_from_an_API <number>")
-        else:
-            userdata = r.get(f"https://jsonplaceholder.typicode.com/users/{id}")
-            todouser = r.get(
-                f"https://jsonplaceholder.typicode.com/\
-    todos/?userId={id}"
+
+    if len(argv) != 2 or not id.isdigit():
+        print("Usage: ./0-gather_data_from_an_API <number>")
+    else:
+        userdata = r.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+        todouser = r.get(
+            f"https://jsonplaceholder.typicode.com/\
+todos/?userId={id}"
             )
-            user = json.loads(userdata.text)
-            todo = json.loads(todouser.text)
-            c_completed_task = [task for task in todo if task["completed"] is True]
-            print(
-                f"Employee {user['name']} is done with \
-    tasks({len(c_completed_task)}/{len(todo)}):"
-            )
-            for t in c_completed_task:
-                print("\t " + t["title"])
-    except Exception as e:
-        print(e)
+        user = json.loads(userdata.text)
+        todo = json.loads(todouser.text)
+        c_completed_task = [task for task in todo if task["completed"] is True]
+        print(
+            f"Employee {user.get('name')} is done with \
+tasks({len(c_completed_task)}/{len(todo)}):"
+        )
+        for t in c_completed_task:
+            print("\t " + t.get("title"))
