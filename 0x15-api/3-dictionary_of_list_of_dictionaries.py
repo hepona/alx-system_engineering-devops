69e5966ea3a8033fd2dc1e2c2d9a45386238e64a
hepona <hepona10@gmail.com>
2024-02-20 10:27:21 +0100

    3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,23 +6,29 @@ from sys import argv
 
 if __name__ == "__main__":
     filename = "todo_all_employees.json"
-
+    data = {}
     userdata = r.get(f"https://jsonplaceholder.typicode.com/users/")
-    todouser = r.get(
-        f"https://jsonplaceholder.typicode.com/\
-todos/?userId="
-    )
+    todouser = r.get(f"https://jsonplaceholder.typicode.com/todos")
     user = json.loads(userdata.text)
     todo = json.loads(todouser.text)
+    # for i in user:
+    #     print(i.get("id"))
+    # for t, u in zip(todo, user):
+    #     data[u.get("id")]= [
+    #         {
+    #             "task": t.get("title"),
+    #             "completed": t.get("completed"),
+    #             "username": u.get("username"),
+    #         }
+    #     ]
+    #     print(data)
     with open(filename, "w") as f:
-        data = {
-            i.get("userId"): [
+        for t, u in zip(todo, user):
+            data[u.get("id")] = [
                 {
-                    "task": i.get("title"),
-                    "completed": i.get("completed"),
-                    "username": user["username"],
+                    "task": t.get("title"),
+                    "completed": t.get("completed"),
+                    "username": u.get("username"),
                 }
             ]
-            for i in todo
-        }
-        json.dump(data, f)
+            json.dump(data, f)
