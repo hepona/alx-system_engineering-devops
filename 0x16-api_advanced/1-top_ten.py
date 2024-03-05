e129e6e1b00be67b6082295b14bb81b6ab6d5bcf
hepona <hepona10@gmail.com>
2024-03-05 17:05:49 +0100

    1 wip

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""top ten"""
+import requests
+
+def top_ten(subreddit):
+    """top ten posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    res = requests.get(url)
+    if res.status_code != 200:
+        return None
+    data = res.json()
+    top = []
+    for i in range(10):
+        top.append(data["data"]["children"][i]["data"]["title"])
+    return print(top)
