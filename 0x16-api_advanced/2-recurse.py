d593bf4d749d1778060f5f138aa527a5f7c834be
hepona <hepona10@gmail.com>
2024-03-05 23:17:34 +0100

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Recurse it"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], c=0):
+    """Prints the titles of the first 10 hot posts from a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Toto"}
+    res = requests.get(url, headers=headers)
+
+    if res.status_code != 200:
+        return
+    data = res.json()
+    if c <= 10:
+        hot_list.append(data["data"]["children"][c]["data"]["title"])
+        recurse(subreddit, hot_list, c+1)
+    return hot_list
+    # top = [post["data"]["title"] for post in data["data"]["children"][:10]]
+
+    # for title in top:
+    #     print(title)
