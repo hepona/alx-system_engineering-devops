76b1554af97c9b0497c5b6d521ed12e4539fc03c
hepona <hepona10@gmail.com>
2024-03-14 13:31:52 +0000

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
-exec {"ulimit":
-  command =>"sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 1000\"/' /etc/default/nginx",
-  provider => "shell",
+exec { 'ulimit':
+  command  =>"sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 1000\"/' /etc/default/nginx",
+  provider => 'shell',
 }
-exec {"restar nginx":
-  command => "service nginx restart",
-  provider => "shell",
+exec { 'restar nginx':
+  command  => 'service nginx restart',
+  provider => 'shell',
 }
